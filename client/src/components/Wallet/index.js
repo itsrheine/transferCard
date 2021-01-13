379d2f3dc49aa4bfd685fb21e0f4d48eda45eb05
daejo <daejo@icloud.com>
2021-01-12 22:35:54 -0800

    updates wallet to use reedux

--- client/src/components/Wallet/index.js
+++ client/src/components/Wallet/index.js
@@ -8,11 +8,13 @@ import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/react-hooks';
 import './style.css';
+import { useDispatch, useSelector } from "react-redux";
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Wallet = () => {
-    const [state, dispatch] = useStoreContext();
+    const state = useSelector(state => state);
+    const dispatch = useDispatch();
     const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
     
     useEffect(() => {
