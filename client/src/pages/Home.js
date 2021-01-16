50f8ba6ae035e331139fd68c76a3f9e39575cedd
Rheine N <mtiamsic@gmail.com>
2021-01-15 22:36:15 -0800

    fixed home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ProductList from "../components/ProductList";
-<<<<<<< HEAD
 import Wallet from '../components/Wallet';
-=======
-import Cart from '../components/Wallet';
->>>>>>> develop
 
 
 const Home = () => {
