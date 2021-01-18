e08218e9072d36cc8627b8bf5ca6b4df77a0cbf4
Rheine N <mtiamsic@gmail.com>
2021-01-18 15:50:36 -0800

    created profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,7 +4,24 @@ const Profile = () => {
 
   return (
     <div className="container">
-
+      <div class="row">
+        <div class="column">
+          <h2>User Info</h2>
+          <p>Some text..</p>
+        </div>
+        <div class="column">
+          <h2>Order History</h2>
+          <p>Some text..</p>
+        </div>
+        <div class="column">
+          <h2>Payment Method</h2>
+          <p>Some text..</p>
+        </div>
+        <div class="column">
+          <h2>Social Media</h2>
+          <p>Some text..</p>
+        </div>
+      </div>
     </div>
   );
 };
