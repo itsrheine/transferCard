6b66eb4c469cbc577033e419b2321bcd758ce19c
Rheine N <mtiamsic@gmail.com>
2021-01-19 19:26:01 -0800

    update User Info

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
 import { useDispatch, useSelector } from "react-redux";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
@@ -11,8 +10,7 @@ function ProductItem(item) {
     image,
     name,
     _id,
-    price,
-    quantity
+    price
   } = item;
   const state = useSelector(state => state);
   const dispatch = useDispatch();
