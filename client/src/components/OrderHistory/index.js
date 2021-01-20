8e54844d4f5115a24866b6009b2678f7c3692c43
Rheine N <mtiamsic@gmail.com>
2021-01-19 20:26:40 -0800

    created FAQ 1

--- client/src/components/OrderHistory/index.js
+++ client/src/components/OrderHistory/index.js
@@ -4,36 +4,28 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_USER } from "../../utils/queries";
 
 function OrderHistory() {
-  const { data } = useQuery(QUERY_USER);
-  let user;
+    const { data } = useQuery(QUERY_USER);
+    let user;
 
-  if (data) {
-    user = data.user;
-  }
+    if (data) {
+        user = data.user;
+        console.log(user);
+    }
 
-  return (
-    <>
-        {user ? (
-          <>
-            {user.orders.map((order) => (
-              <div key={order._id} className="my-2">
-                <h3>{new Date(parseInt(order.purchaseDate)).toLocaleDateString()}</h3>
-                <div className="flex-row">
-                  {order.products.map(({ name, price }, index) => (
-                    <div key={index} className="card px-1 py-1">    
-                        <p>{name}</p>
-                      <div>
-                        <span>${price}</span>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
+    return (
+        <>
+            {user ? (
+                <>
+                    {user.orders.map((order) => (
+                        <div key={order._id} className="card px-1 py-1">
+                            <p>Date Purchased: {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}</p>
+                            {order.products.map(({ name, price }, index) => (
+                                <p key={index}>Ticket: {name} {price}</p>
+                            ))}
+                        </div>
+                ))}
           </>
         ) : null}
-
     </>)
-
 };
 export default OrderHistory;
--- client/src/pages/Faq.js
+++ client/src/pages/Faq.js
@@ -6,8 +6,15 @@ const Profile = () => {
     <div className="container">
       <div class="row">
         <div class="column">
-          <h2>Payment Method</h2>
-          <p>Some text..</p>
+          <h2>How to Purchase Tickets - Online and Mobile</h2>
+          <p><i>For your convenience and for your safety, trasferCard is now exclusively accepting cashless payments on web and mobile application.</i></p>
+          <p>Purchase your tickets at any time on URLHERE with:
+            <ul>
+              <li>Valid credit card (Visa, Mastercard®, American Express, Discover®)</li>
+              <li>Google Pay</li>
+            </ul>
+          </p>
+          <p>*We currently do not accept travelers cheques or money orders.</p>
         </div>
         <div class="column">
           <h2>Social Media</h2>
